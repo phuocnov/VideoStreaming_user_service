ab021574c3dcaa6f02ec84da14e0d42d01f3c6ac
phuocnov <phuocnguyenhuu806@gmail.com>
2024-06-09 02:00:28 +0700

    create user model

---
+++ src/models/User.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document, Model } from "mongoose";
+
+interface IUser extends Document {
+  username: string;
+  password: string;
+  email: string;
+  createAt: Date;
+}
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User: Model<IUser> = mongoose.model<IUser>("User", userSchema);
+
+export default User;
