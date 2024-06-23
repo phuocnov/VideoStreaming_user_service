98e65a63e3a7a8316d579e3973ed1dd5911de1aa
phuocnov <phuocnguyenhuu806@gmail.com>
2024-06-23 16:50:36 +0700

    TEST:: add test suite for sign in

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -42,5 +42,57 @@ describe("Auth service", () => {
       expect(jwt.sign).toHaveBeenCalledWith({ id: mockUser._id }, "secret");
       expect(result).toEqual({ user: mockUser, token: "mockToken" });
     });
+
+    it("should throw an error if the email already exists", async () => {
+      (User.findOne as jest.Mock).mockResolvedValueOnce({ email: "mockEmail" });
+      await expect(
+        signup("mockUsername", "mockEmail", "mockPassword")
+      ).rejects.toThrow("Email already exists");
+    });
+  });
+
+  describe("signin", () => {
+    it("should return a token if the user is found and the password is correct", async () => {
+      const mockUser = {
+        _id: "mockId",
+        username: "mockUsername",
+        email: "mockEmail",
+        password: "mockHashedPassword",
+      };
+
+      (User.findOne as jest.Mock).mockResolvedValueOnce(mockUser);
+      (bcrypt.compare as jest.Mock).mockResolvedValueOnce(true);
+      (jwt.sign as jest.Mock).mockReturnValue("mockToken");
+
+      const result = await signin("mockEmail", "mockPassword");
+
+      expect(User.findOne).toHaveBeenCalledWith({
+        username: "mockEmail",
+        $or: [{ email: "mockEmail" }],
+      });
+      expect(bcrypt.compare).toHaveBeenCalledWith(
+        "mockPassword",
+        "mockHashedPassword"
+      );
+      expect(jwt.sign).toHaveBeenCalledWith({ id: mockUser._id }, "secret");
+      expect(result).toEqual({ user: mockUser, token: "mockToken" });
+    });
+
+    it("should throw an error if the user is not found", async () => {
+      (User.findOne as jest.Mock).mockResolvedValueOnce(null);
+      await expect(signin("mockEmail", "mockPassword")).rejects.toThrow(
+        "User not found"
+      );
+    });
+
+    it("should throw an error if the password is incorrect", async () => {
+      (User.findOne as jest.Mock).mockResolvedValueOnce({
+        password: "mockHashedPassword",
+      });
+      (bcrypt.compare as jest.Mock).mockResolvedValueOnce(false);
+      await expect(signin("mockEmail", "mockPassword")).rejects.toThrow(
+        "Password is incorrect"
+      );
+    });
   });
 });
